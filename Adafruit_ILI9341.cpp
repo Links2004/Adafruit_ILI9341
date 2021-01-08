ac755d6ca4a9b9fa49d9387781d6ad17460ff6c2
Links <help.markus+gitk@gmail.com>
2021-01-08 09:51:45 +0100

    pgmspace.h for ESP32

--- Adafruit_ILI9341.cpp
+++ Adafruit_ILI9341.cpp
@@ -16,7 +16,7 @@
  ****************************************************/
 
 #include "Adafruit_ILI9341.h"
-#ifdef ESP8266
+#if defined(ESP8266) || defined(ESP32) || defined(ESP31B)
 #include <pgmspace.h>
 #else
 #include <avr/pgmspace.h>
